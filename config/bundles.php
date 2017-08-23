871ac11338dfa6a1bfaa03f848739d7ebcacfe2a
FishmanDev <fishman.code@gmail.com>
2017-08-23 06:51:18 +0300

    composer req sensio/framework-extra-bundle twig asset

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
 ];
