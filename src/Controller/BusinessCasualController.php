f1fd5987306045578fdde374bbcc4cc2e2b6a8e4
FishmanDev <fishman.code@gmail.com>
2017-09-01 12:16:37 +0300

    Create an 'index' controller and template

--- src/Controller/BusinessCasualController.php
+++ src/Controller/BusinessCasualController.php
@@ -11,4 +11,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  */
 class BusinessCasualController extends Controller
 {
+    /**
+     * @Route("/business-casual", name="business-casual_home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('business-casual/index.html.twig');
+    }
 }
