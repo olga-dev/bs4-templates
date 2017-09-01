ba8be452c5ca596b95846d9f34147817677c7e23
FishmanDev <fishman.code@gmail.com>
2017-09-01 12:16:05 +0300

    Create a 'Business Casual' controller

---
+++ src/Controller/BusinessCasualController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class BusinessCasualController
+ * @package App\Controller
+ */
+class BusinessCasualController extends Controller
+{
+}
